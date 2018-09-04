01b0a433940e44b1bdd9d7599a751dfe4b4907ab
sunnyf1sh <sunnyf1sh@sina.com>
2018-09-04 17:20:15 +0800

    admin module

---
+++ application/admin/model/Admin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/9/4
+ * Time: 16:52
+ */
+namespace app\admin\model;
+use think\Model;
+
+class Admin extends Model{
+    protected $pk = 'id';
+
+}
